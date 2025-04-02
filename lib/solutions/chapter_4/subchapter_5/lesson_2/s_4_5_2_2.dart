00cb47853da1f9d28b738e693aea5bd838d920e5
melikeaygul <melikeaygul0810@gmail.com>
2025-04-02 12:20:32 +0200

    Aufgabe 4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,21 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Hello Melike!"),
+        SizedBox(height: 20),
+        ElevatedButton(onPressed: null, child: Text("Klick mich"))
+      ],
+    );
   }
 }
